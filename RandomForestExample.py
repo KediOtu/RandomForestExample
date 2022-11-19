0d95d3ca5b12d2267b7e7aa5b25f7f3c7ee15a48
Süheyla Kabak <74386450+KediOtu@users.noreply.github.com>
2022-11-19 20:22:21 +0300

    Update RandomForestExample.py

--- RandomForestExample.py
+++ RandomForestExample.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-random_state = int(input())
+random_state = int(input()) #we will use it as random_state while generating training sets and test sets
 n = int(input())
 rows = []
 for i in range(n):
@@ -11,8 +11,10 @@ for i in range(n):
 X = np.array(rows)
 y = np.array([int(a) for a in input().split()])
 
+#generating training sets and test sets:
 X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=random_state)
 
+#generating the random forest with 5 decision trees
 rf = RandomForestClassifier(n_estimators=5)
 rf.fit(X_train,y_train)
 
